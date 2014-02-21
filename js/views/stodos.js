ffb7d6a82a672cdbff19311ca9cdfd12bc9b2f98
phoenixbox <shane@quickleft.com>
2014-02-20 21:52:12 -0700

    added the toggle and clear event handlers on the todo items via the appview incl removal from the defined localStorage

--- js/views/stodos.js
+++ js/views/stodos.js
@@ -6,19 +6,43 @@ app.StodoView = Backbone.View.extend({
   template: _.template( $('#item-template').html() ),
 
   events: {
+    'click .toggle': 'toggleCompleted',
     'dblclick label': 'edit',
+    'click .destory': 'clear',
     'keypress .edit': 'updateOnEnter',
     'blur .edit': 'close'
   },
 
   initialize: function() {
     this.listenTo(this.model, 'change', this.render);
+    this.listenTo(this.model, 'destroy', this.remove);
+    this.listenTo(this.model, 'visible', this.toggleVisible);
   },
 
   render: function() {
     this.$el.html( this.template( this.model.toJSON() ) );
+
+    this.$el.toggleClass('completed', this.model.get('completed') );
+    this.toggleVisible();
+
     this.$input = this.$('.edit');
     return this;
+  },
+
+  toggleVisible: function() {
+    this.$el.toggleClass('hidden', this.isHidden());
+  },
+
+  isHidden: function() {
+    var isCompleted = this.model.get('completed');
+    return (
+      (!isCompleted && app.StodoFilter==='completed')||
+      (isCompleted && app.StodoFilter ==='active')
+    );
+  },
+
+  toggleCompleted: function() {
+    this.model.toggle();
   },
 
   edit: function() {
@@ -39,5 +63,9 @@ app.StodoView = Backbone.View.extend({
     if ( event.which === ENTER_KEY ) {
       this.close();
     }
+  },
+
+  clear: function() {
+    this.model.destroy();
   }
 });
