31dac7f73ed421a11b28df971ed75313ed19ba06
phoenixbox <shane@quickleft.com>
2014-02-20 20:15:28 -0700

    stodos collection setup with comparator functions and filter functions

---
+++ js/collections/stodos.js
@@ -0,0 +1,30 @@
+var app = app || {}
+
+var StodoList = Backbone.Collection.extend({
+  model: app.Stodo,
+
+  localStorage: new Backbone.LocalStorage('stodos-backbone'),
+
+  completed: function(){
+    return this.filter(function(stodo){
+      return stodo.get('completed');
+    });
+  },
+  // Apply defines the context of this within the functions scope
+  remaining: function(){
+    return this.without.apply( this, this.completed() );
+  },
+
+  nextOrder: function(){
+    if (!this.length){
+      return 1;
+    }
+    return this.last().get('order')+1;
+  },
+
+  comparator: function(){
+    return stodo.get('order');
+  },
+});
+
+app.Stodos = new StodoList();
