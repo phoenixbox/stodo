d7fc7ef6b4bc36aacd0c83255446eb533f31f4cd
phoenixbox <shane@quickleft.com>
2014-02-20 21:00:49 -0700

    added more app view listeners and event callbacks to add remove and filter th e object model and collection

--- js/views/app.js
+++ js/views/app.js
@@ -3,7 +3,13 @@ var app = app || {};
 app.AppView = Backbone.View.extend({
   el : '#todoapp',
 
-  statusTemplate: _.template( $('#stats-template').html() ),
+  statsTemplate: _.template( $('#stats-template').html() ),
+
+  events: {
+    'keypress #new-todo': 'createOnEnter',
+    'click #clear-completed': 'clearCompleted',
+    'click #toggle-all': 'toggleAllComplete',
+  },
 
   initialize: function() {
     // Cache the DOM elements - found relative to el
@@ -15,6 +21,38 @@ app.AppView = Backbone.View.extend({
     // Event Bindings
     this.listenTo(app.Stodos, 'add', this.addOne);
     this.listenTo(app.Stodos, 'reset', this.addAll);
+
+    this.listenTo(app.Stodos, 'change:completed', this.filterOne);
+    this.listenTo(app.Stodos, 'filter', this.filterAll);
+    this.listenTo(app.Stodos, 'all', this.render);
+
+    // Load items which may be in LocalStorage
+    app.Stodos.fetch();
+  },
+
+  render: function(){
+    var completed = app.Stodos.completed().length;
+    var remaining = app.Stodos.remaining().length;
+
+    if (app.Stodos.length) {
+      this.$main.show();
+      this.$footer.show();
+
+      this.$footer.html(this.statsTemplate({
+        completed: completed,
+        remaining: remaining
+      }));
+
+      this.$('#filters li a')
+      .removeClass('selected')
+      .filter('[href="#/' + ( app.TodoFilter || '' ) + '"]')
+      .addClass('selected');
+    } else {
+      this.$main.hide();
+      this.$footer.hide();
+    }
+
+    this.allCheckbox.checked = !remaining;
   },
 
   // Append new list view items on fired of the add event
@@ -28,6 +66,44 @@ app.AppView = Backbone.View.extend({
     this.$('#todo-list').html('');
     // Can use this because listenTo() implicitly set the callback context to the view on bind creation
     app.Stodos.each(this.addOne, this);
-  }
+  },
 
+  filterOne: function (stodo) {
+    stodo.trigger('visible');
+  },
+
+  filterAll: function() {
+    app.Stodos.each(this.filterOne, this);
+  },
+
+  newAttributes: function() {
+    return {
+      title: this.$input.val().trim(),
+      order: app.Stodos.nextOrder(),
+      completed: false
+    }
+  },
+
+  createOnEnter: function(event) {
+    if (event.which !== ENTER_KEY || !this.$input.val().trim() ) {
+      return;
+    }
+    app.Stodos.create( this.newAttributes() ) ;
+    this.$input.val('');
+  },
+
+  clearCompleted: function() {
+    _.invoke(app.Stodos.completed(), 'destroy');
+    return false;
+  },
+
+  toggleAllComplete: function() {
+    var completed = this.allCheckbox.checked;
+
+    app.Stodos.each(function(todo){
+      todo.save({
+        'completed': completed
+      });
+    });
+  }
 });
